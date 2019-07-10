db2a80f03c9a0c3f66f9afc95c5200e17d6fe6df
onetrckchris <onetrckchris@gmail.com>
2019-07-10 13:25:16 -0700

    getting APOD data and holding it on a hook.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 function App() {
+  const [nasaData, setNasaData] = useState({});
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=fB4ysTncHrP86v3bq2wLZ4Vw4v4bgHm3Ld0KewbW')
+      .then(response => {
+        setNasaData(response.data);
+      })
+      .catch(error => console.log(error))
+  }, [])
+
+  console.log(nasaData)
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      
     </div>
   );
 }
